6c7f20826fad5dbd01f69cd5e15f4b788192eab8
Na1x-dev <na1x.1024@gmail.com>
2023-12-06 22:10:57 +0300

    feat: changing themes

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,21 +1,85 @@
 function CallPrint(strid) {
- var prtContent = document.querySelector(strid);
-   var prtCSS1 = '<style>.table {width: 100%;margin-bottom: 20px;border: 1px solid #dddddd;border-collapse: collapse; }.table th {font-weight: bold;padding: 5px;background: #efefef;border: 1px solid #dddddd;}.table td {border: 1px solid #dddddd;padding: 5px;}</style>';
-   var prtCSS2 = '<style>.table th{font-weight: bold;padding: 5px;background: #efefef;border: 1px solid #333333;}</style>';
-   var prtCSS3 = '<style>.table td{font-weight: bold;padding: 5px;background: #efefef;border: 1px solid #333333;}</style>';
-   var WinPrint = window.open('gg','wp','left=50,top=50,width=800,height=640,toolbar=0,scrollbars=1,status=0');
-   WinPrint.document.write(prtCSS1);
-   WinPrint.document.write('<table class="table">');
-   WinPrint.document.write(prtContent.innerHTML);
-   while(WinPrint.document.querySelector('.open-dialog')!=null){
+    var prtContent = document.querySelector(strid);
+    var prtCSS1 = '<style>.table {width: 100%;margin-bottom: 20px;border: 1px solid #dddddd;border-collapse: collapse; }.table th {font-weight: bold;padding: 5px;background: #efefef;border: 1px solid #dddddd;}.table td {border: 1px solid #dddddd;padding: 5px;}</style>';
+    var prtCSS2 = '<style>.table th{font-weight: bold;padding: 5px;background: #efefef;border: 1px solid #333333;}</style>';
+    var prtCSS3 = '<style>.table td{font-weight: bold;padding: 5px;background: #efefef;border: 1px solid #333333;}</style>';
+    var WinPrint = window.open('gg', 'wp', 'left=50,top=50,width=800,height=640,toolbar=0,scrollbars=1,status=0');
+    WinPrint.document.write(prtCSS1);
+    WinPrint.document.write('<table class="table">');
+    WinPrint.document.write(prtContent.innerHTML);
+    while (WinPrint.document.querySelector('.open-dialog') != null) {
         WinPrint.document.querySelector('.open-dialog').parentElement.parentElement.remove();
-   }
-   WinPrint.document.write('</table>');
+    }
+    WinPrint.document.write('</table>');
 
-   WinPrint.document.close();
-   WinPrint.focus();
-   WinPrint.print();
-   WinPrint.close();
-   prtContent.innerHTML=strOldOne;
+    WinPrint.document.close();
+    WinPrint.focus();
+    WinPrint.print();
+    WinPrint.close();
+    prtContent.innerHTML = strOldOne;
 }
 
+let switcherButtonLight = document.querySelector(".switcher-button-light");
+let switcherButtonDark = document.querySelector(".switcher-button-dark");
+
+
+switcherButtonDark.onclick = () => {
+    setTheme(true);
+}
+
+switcherButtonLight.onclick = () => {
+    setTheme(false);
+}
+
+function setTheme(themeFlag) {
+    if (themeFlag) {
+        switcherButtonDark.style.backgroundColor = "var(--bg-color-1)";
+        switcherButtonLight.style.backgroundColor = "var(--bg-color-2)";
+        document.documentElement.style.setProperty('--bg-color-1', '#222');
+        document.documentElement.style.setProperty('--bg-color-2', '#333');
+        document.documentElement.style.setProperty('--bg-color-3', '#444');
+        document.documentElement.style.setProperty('--bg-color-4', '#555');
+        document.documentElement.style.setProperty('--font-color-1', '#ddd');
+        document.documentElement.style.setProperty('--font-color-2', '#eee');
+        document.documentElement.style.setProperty('--font-color-3', '#fff');
+        document.documentElement.style.setProperty('--border-color', '#777');
+        localStorage.setItem('isDarkTheme', true);
+        console.log(localStorage.getItem('isDarkTheme'));
+    } else {
+        switcherButtonLight.style.backgroundColor = "var(--bg-color-1)";
+        switcherButtonDark.style.backgroundColor = "var(--bg-color-2)";
+        document.documentElement.style.setProperty('--bg-color-1', '#ccc');
+        document.documentElement.style.setProperty('--bg-color-2', '#ddd');
+        document.documentElement.style.setProperty('--bg-color-3', '#eee');
+        document.documentElement.style.setProperty('--bg-color-4', '#fff');
+        document.documentElement.style.setProperty('--font-color-1', '#222');
+        document.documentElement.style.setProperty('--font-color-2', '#333');
+        document.documentElement.style.setProperty('--font-color-3', '#444');
+        document.documentElement.style.setProperty('--border-color', '#777');
+        localStorage.setItem('isDarkTheme', false);
+        console.log(localStorage.getItem('isDarkTheme'));
+    }
+}
+
+function setStartTheme() {
+    setTheme(localStorage.getItem('isDarkTheme'));
+}
+
+function initLocalStorage(){
+    if(localStorage.getItem('isDarkTheme') == null ){
+        localStorage.setItem('isDarkTheme', false);
+    }
+}
+
+console.log(localStorage.getItem('isDarkTheme'));
+setStartTheme();
+// initLocalStorage();
+
+
+for (let elem of document.getElementsByTagName('a')) {
+    elem.style.transitionDuration = '0.2s';
+}
+
+for (let elem of document.getElementsByTagName('button')) {
+    elem.style.transitionDuration = '0.2s';
+}
